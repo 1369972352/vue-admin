0d200013421612a2866bb609f411e5da9fe73ba1
Admin <1369972352@qq.com>
2020-02-23 21:49:53 +0800

    构建项目，配置全局样式，路由，elementui

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+      这里是玩一夜
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#login{
+    height: 50vh;
+    background-color: #344a5f;
+}
+</style>
